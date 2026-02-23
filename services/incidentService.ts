e2434ec1a91743f8a3b771a545e67f11a3739156
Kervcodes <kervintz84@hotmail.com>
2026-02-23 00:37:20 -0500

    feat: implement updateStatus service function

--- services/incidentService.ts
+++ services/incidentService.ts
@@ -10,6 +10,12 @@ type CreateIncidentInput = {
     ownerId: string;
 }
 
+type UpdateStatusInput = {
+  incidentId: string;
+  newStatus: Status;
+  userId: string;
+};
+
 const SLA_MAP: Record<Severity, number> = {
     LOW: 240,
     MEDIUM: 120,
@@ -39,4 +45,100 @@ export const createIncident = async (input: CreateIncidentInput) => {
 
     return incident;
 
+}
+
+
+export const updateStatus = async (input: UpdateStatusInput) => {
+    const incident = await prisma.incident.findUnique({
+        where: { id: input.incidentId },
+    })
+
+    if (!incident) {
+        throw new Error(`Incident not found!`)
+    }
+
+    const currentStatus = incident.status;
+    const newStatus = input.newStatus;
+
+    let acknowledgedAt = incident.acknowledgedAt;
+    let resolvedAt = incident.resolvedAt;
+    let closedAt = incident.closedAt;
+    let currentSlaStartAt = incident.currentSlaStartAt;
+
+    switch(currentStatus) {
+        case Status.OPEN:
+            if (newStatus === Status.INVESTIGATING) {
+                acknowledgedAt = new Date();
+            } else if (newStatus === Status.DISCARDED) {
+                // nothing happened here
+            } else {
+                throw new Error(`Invalid transistion from OPEN to ${newStatus}`);
+            }
+            break;
+        case Status.INVESTIGATING:
+            if (newStatus === Status.IDENTIFIED) {
+                // nothing happened here
+            } else if(newStatus === Status.DISCARDED) {
+                // nothing happened here
+            } else {
+                throw new Error(`Invalid transistion from INVESTIGATING to ${newStatus}`);
+            }
+            break;
+        case Status.IDENTIFIED:
+            if (newStatus === Status.MITIGATED) {
+                // nothing happened here
+            } else {
+                throw new Error(`Invalid transistion from IDENTIFIED to ${newStatus}`);
+            }
+            break;
+        case Status.MITIGATED:
+            if (newStatus === Status.RESOLVED) {
+                resolvedAt = new Date();
+            } else {
+                throw new Error(`Invalid transistion from MITIGATED to ${newStatus}`);
+            }
+            break;
+        case Status.RESOLVED:
+            if (newStatus === Status.CLOSED) {
+                closedAt = new Date();
+            } else if(newStatus === Status.INVESTIGATING) {
+                currentSlaStartAt = new Date()
+                acknowledgedAt = null;
+                resolvedAt = null;
+                closedAt = null;
+            } else {
+                throw new Error(`Invalid transistion from RESOLVED to ${newStatus}`)
+            }
+            break;
+        case Status.CLOSED:
+            if (newStatus === Status.INVESTIGATING) {
+                currentSlaStartAt = new Date()
+                acknowledgedAt = null;
+                resolvedAt = null;
+                closedAt = null;
+            } else {
+                throw new Error(`Invalid transistion from CLOSED to ${newStatus}`);
+            }
+            break;
+        case Status.DISCARDED:
+            throw new Error(`Cannot transition from DISCARDED`);
+
+        default:
+            throw new Error(`Unhandled current status: ${currentStatus}`);
+
+    }
+
+    const updatedIncident = await prisma.incident.update({
+        where: { id: incident.id },
+        data: {
+        status: newStatus,
+        acknowledgedAt,
+        resolvedAt,
+        closedAt,
+        currentSlaStartAt,
+        },
+    });
+
+    return updatedIncident;
+    
 }
